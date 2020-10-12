3df0c0db6b92ef9c47ca6e34a8d2f9e3d64d836a
samkeen <samkeen@lululemon.com>
2020-10-11 17:03:50 -0700

    first commit

---
+++ Assets/Scripts/GameBoard.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// https://generalistprogrammer.com/game-design-development/unity-spawn-prefab-at-position-tutorial/
+/// https://answers.unity.com/questions/1590160/how-to-spawn-objects-within-a-planes-dimensions.html
+/// </summary>
+public class GameBoard : MonoBehaviour
+{
+    [SerializeField] private Transform plane;
+    [SerializeField] private GameObject fruit;
+
+    // Plane Properties
+    private Bounds planeMeshBounds;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        planeMeshBounds = plane.GetComponent<MeshFilter>().mesh.bounds;
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void Spawn()
+    {
+        Vector3 randPos = Vector3.zero;
+        randPos.x = Random.Range(planeMeshBounds.min.x, planeMeshBounds.max.x);
+        randPos.z = Random.Range(planeMeshBounds.min.z, planeMeshBounds.max.z);
+        Debug.Log($"Placing apple at {randPos}");
+        GameObject obj = Instantiate (fruit, randPos, Quaternion.identity,plane);
+
+    }
+}
---
+++ Assets/Scripts/SnakeHead.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeHead : MonoBehaviour
+{
+    [SerializeField] private float snakeSpeed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        transform.Translate(move * Time.deltaTime * snakeSpeed);
+        // Debug.Log($"y = {move.y}");
+        // controller.Move(move * Time.deltaTime * snakeSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"Trigger - Other: {other.name}");
+    }
+}
