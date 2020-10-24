7095dab814276ca7608b80d6aa0fa69694cc7121
samkeen <samkeen@lululemon.com>
2020-10-24 10:34:00 -0700

    added score counter - score counter listens for apple eaten event https://app.clickup.com/t/ewq3nw

---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreBoard : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private int pointsForEatenApple = 10;
+    
+
+
+    private void Start()
+    {
+        scoreText.text = "0";
+        // subscribe to Apple.eatenEvent
+        FindObjectOfType<Apple>().EatenEvent += OnAppleEaten;
+    }
+    
+    private void OnAppleEaten()
+    {
+        Debug.Log("**ScoreBoard saw apple eaten event**");
+        var score = Int32.Parse(scoreText.text);
+        score += pointsForEatenApple;
+        scoreText.text = score.ToString();
+    }
+}
