466b753fc419f842d5985e78537ffde2e8e1aabe
samkeen <samkeen@lululemon.com>
2020-11-09 21:32:19 -0800

    added score on end menu

---
+++ Assets/Scripts/EndMenu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndMenu : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI score;
+
+
+    private void OnEnable()
+    {
+        score.text = $"{score.text} {PlayerStats.Points}";
+    }
+}
