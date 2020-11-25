67d2ec9833144f5cf3f1544b0293c8bac012b1c9
samkeen <samkeen@lululemon.com>
2020-11-24 20:55:26 -0800

    hooked up volume slider
    https://app.clickup.com/t/f8xe2d

---
+++ Assets/Scripts/SetVolume.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+/// <summary>
+/// https://gamedevbeginner.com/the-right-way-to-make-a-volume-slider-in-unity-using-logarithmic-conversion/
+/// </summary>
+public class SetVolume : MonoBehaviour
+{
+    [SerializeField] private AudioMixer mixer;
+    [SerializeField] private Slider slider;
+    
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
+    }
+
+    public void SetLevel (float sliderValue)
+    {
+        mixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", sliderValue);
+    }
+}
